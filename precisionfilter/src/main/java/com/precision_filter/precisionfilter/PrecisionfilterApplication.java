b7934d267afdbc16ec01d3e788c151925b578da1
matayl26 <matayl26@ncsu.edu>
2026-02-14 11:48:14 -0500

    initial commit with filestructure for springboot

---
+++ precisionfilter/src/main/java/com/precision_filter/precisionfilter/PrecisionfilterApplication.java
@@ -0,0 +1,13 @@
+package com.precision_filter.precisionfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecisionfilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecisionfilterApplication.class, args);
+	}
+
+}
---
+++ precisionfilter/src/test/java/com/precision_filter/precisionfilter/PrecisionfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precision_filter.precisionfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecisionfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
