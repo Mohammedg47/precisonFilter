53023fe1015c9b84c4b82554454d9a496634abe0
zeke75895 <111441069+zeke75895@users.noreply.github.com>
2026-02-14 13:49:28 -0500

    Added html to index file and script.js

---
+++ precisionfilter/src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+function storeValue() {
+    // Select the input element
+    const inputElement = document.getElementById('text-box');
+
+    // Access the value property and store it in a variable
+    const inputValue = inputElement.value;
+}
