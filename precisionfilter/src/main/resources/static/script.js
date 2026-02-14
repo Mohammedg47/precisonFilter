53f014b88ad1c239dc709b7173080a730fcc0dff
matayl26 <matayl26@ncsu.edu>
2026-02-14 18:02:14 -0500

    messing with script and index

--- precisionfilter/src/main/resources/static/script.js
+++ precisionfilter/src/main/resources/static/script.js
@@ -1,7 +1,22 @@
 function storeValue() {
-    // Select the input element
     const inputElement = document.getElementById('text-box');
-
-    // Access the value property and store it in a variable
     const inputValue = inputElement.value;
+    analyzeEmail(inputValue);
+}
+
+function analyzeEmail(emailText) {
+  fetch("/analyzeEmail", {
+    method: "POST", 
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: emailText })
+  })
+  .then(res => res.json())
+  .then(data => {
+    // Display AI response in the aiOutput div
+    document.getElementById("aiOutput").innerText = data.content;
+  })
+  .catch(err => {
+    console.error("Error analyzing email:", err);
+    document.getElementById("aiOutput").innerText = "Error analyzing email.";
+  });
 }
